cb59a5bbc3787202f046100e671adc49dcf54d9d
bibekshrestha450 <barunshrestha450@gmail.com>
2025-05-19 22:07:25 +0545

    he

---
+++ file handelling/file.py
@@ -0,0 +1,16 @@
+# # this is for addpen if i run it it will just keep adding it
+
+# f = open("file.txt", "a")
+# f.write("Hello, world!")
+# f.close()
+
+
+# f = open("data.txt", "r")  # ❌ Error if file doesn't exist
+
+
+# f = open("newfile.txt", "w")  # ✅ File is created
+
+# f = open("log.txt", "a")  # ✅ File is created or used
+
+# f = open("newonly.txt", "x")  # ❌ Error if already exists
+
